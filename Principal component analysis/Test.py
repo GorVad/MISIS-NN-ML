92f39ff6ac648d7e4f7a4bf527438a72339a7873
DaxterMann <48520987+DaxterMann@users.noreply.github.com>
2020-10-28 19:13:28 +0300

    PCA 1. Реализован метод PCA для датасета Audit (для теста 3 компонента)

--- Principal component analysis/Test.py
+++ Principal component analysis/Test.py
@@ -1,23 +1,24 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import decomposition
+import pandas as pd
+pd.set_option('precision', 3)
+pd.set_option('display.max_columns', 100)
 
-
-def histogram_intersection(a, b):
-    v = np.minimum(a, b).sum().round(decimals=1)
-    return v
-
+# Получение датасета и построение изначальной матрицы корреляции
 auditDataSet = pd.read_csv(r"D:\PyCharm\MISIS-NN-ML\Principal component analysis\Datasets\audit.csv")
-corrMatrix = plt.matshow(auditDataSet.corr())
+newAuditDataSet = auditDataSet.drop(['Sector_score', 'LOCATION_ID', 'TOTAL', 'numbers','District_Loss', 'History'], axis=1)
+
+corrMatrix = plt.matshow(newAuditDataSet.corr())
 cb = plt.colorbar()
 cb.ax.tick_params(labelsize=14)
+plt.title('Первоначальная матрица корреляции датасета Audit')
 plt.show()
 
-df = pd.DataFrame(auditDataSet).dropna()
+# Получение списка матрицы корреляции для выясления элементов со слабейшей корреляцией
+df = pd.DataFrame(newAuditDataSet).dropna()
 corrM = df.corr()
 np.fill_diagonal(corrM.values, np.nan)
-print(corrM)
-
 order_bottom = np.argsort(corrM.values, axis=1)[:, :3]
 result_bottom = pd.DataFrame(
     corrM.columns[order_bottom],
@@ -26,11 +27,25 @@ result_bottom = pd.DataFrame(
 )
 for x in result_bottom.columns:
     result_bottom[x+"_Val"] = corrM.lookup(corrM.index, result_bottom[x])
-print(result_bottom)
 # print(result_bottom)
-# auditDataSet = pandas.read_csv(r"D:\PyCharm\MISIS-NN-ML\Principal component analysis\Datasets\isolet.csv")
-# plt.matshow(auditDataSet.corr())
-# plt.show()
-# auditDataSet = pandas.read_csv(r"D:\PyCharm\MISIS-NN-ML\Principal component analysis\Datasets\sonar.csv")
-# plt.matshow(auditDataSet.corr())
-# plt.show()
+
+# Выдергивание атрибутов со слабой корреляции из общего датасета
+pcaAuditDataSet = auditDataSet.iloc[:, [16,17, 20, 2, 23, 12, 6, 14]]
+nAuditDataSet = newAuditDataSet.drop(['PROB','RiSk_E', 'Risk_F', 'PARA_A', 'CONTROL_RISK', 'Money_Value', 'Score_B', 'Risk_D'], axis=1)
+# print(nAuditDataSet)
+
+# Реализация PCA и присоединение получившихся столбцов к изначальному датасету
+pcaAuditDataSet = pd.DataFrame(newAuditDataSet).dropna()
+pca = decomposition.PCA(n_components=3)
+pcaAuditDataSet_centered = pcaAuditDataSet - pcaAuditDataSet.mean(axis=0)
+pcaAuditDataSet_transformed = pca.fit_transform(pcaAuditDataSet_centered)
+principalDf = pd.DataFrame(data = pcaAuditDataSet_transformed, columns=['1', '2', '3'])
+# print(principalDf)
+newAuditDataSet = newAuditDataSet.join(principalDf)
+# print(newAuditDataSet)
+
+corrMatrix = plt.matshow(newAuditDataSet.corr())
+cb = plt.colorbar()
+cb.ax.tick_params(labelsize=14)
+plt.title('Матрица корреляции датасета Audit после использования PCA')
+plt.show()
